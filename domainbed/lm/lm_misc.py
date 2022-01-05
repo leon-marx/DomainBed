7f6a8073fa88ea78c40e57d8d8cf7902a0adc22e
leon-marx <gooogleon@gmail.com>
2022-01-05 22:41:14 +0100

    bug fix

--- domainbed/lm/lm_misc.py
+++ domainbed/lm/lm_misc.py
@@ -1,14 +1,13 @@
 import torch
 
 def accuracy(network, loader, weights, device):
-    total = 0
 
     with torch.no_grad():
+        total = 0
         for x, y in loader:
             x["image"] = x["image"].to(device)
             x["domain"] = x["domain"].to(device)
             y = y.to(device)
             _, loss = network.evaluate(minibatches=[(x, y)], return_eval_loss=True)
             total += loss
-
-    return 1 / (1 + total)
+        return 1 / (1 + total)
