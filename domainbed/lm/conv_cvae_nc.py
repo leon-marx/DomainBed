0e1d9c96cb1d602a53667614a27e0a85344a9427
leon-marx <gooogleon@gmail.com>
2022-01-13 11:56:25 +0100

    bug fix regarding grad plotting

--- domainbed/lm/conv_cvae_nc.py
+++ domainbed/lm/conv_cvae_nc.py
@@ -1,6 +1,8 @@
-import torch
 import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
+import numpy as np
 import os
+import torch
 
 
 class Algorithm(torch.nn.Module):
@@ -103,7 +105,7 @@ class LM_CCVAE_NC(Algorithm):
         self.optimizer.zero_grad()
         loss.backward()
         if output_dir is not None:
-            self.plot_grad_flow(output_dir)
+            self.plot_grad_flow(self.named_parameters(), output_dir)
         self.optimizer.step()
 
         if return_train_loss:
@@ -220,21 +222,35 @@ class LM_CCVAE_NC(Algorithm):
             print(f"Unexpected keys in state dict: {unexpected}")
 
     def plot_grad_flow(named_parameters, output_dir):
+        """
+        Plots the gradients flowing through different layers in the net during training.
+        Can be used for checking for possible gradient vanishing / exploding problems.
+        
+        Usage: Plug this function in Trainer class after loss.backwards() as 
+        "plot_grad_flow(self.model.named_parameters())" to visualize the gradient flow
+        """
         ave_grads = []
+        max_grads= []
         layers = []
         for n, p in named_parameters:
             if(p.requires_grad) and ("bias" not in n):
                 layers.append(n)
                 ave_grads.append(p.grad.abs().mean())
+                max_grads.append(p.grad.abs().max())
         plt.figure()
-        plt.plot(ave_grads, alpha=0.3, color="b")
-        plt.hlines(0, 0, len(ave_grads)+1, linewidth=1, color="k" )
+        plt.bar(np.arange(len(max_grads)), max_grads, alpha=0.1, lw=1, color="c")
+        plt.bar(np.arange(len(max_grads)), ave_grads, alpha=0.1, lw=1, color="b")
+        plt.hlines(0, 0, len(ave_grads)+1, lw=2, color="k" )
         plt.xticks(range(0,len(ave_grads), 1), layers, rotation="vertical")
-        plt.xlim(xmin=0, xmax=len(ave_grads))
+        plt.xlim(left=0, right=len(ave_grads))
+        plt.ylim(bottom = -0.001, top=0.02) # zoom in on the lower gradient regions
         plt.xlabel("Layers")
         plt.ylabel("average gradient")
         plt.title("Gradient flow")
         plt.grid(True)
+        plt.legend([Line2D([0], [0], color="c", lw=4),
+                    Line2D([0], [0], color="b", lw=4),
+                    Line2D([0], [0], color="k", lw=4)], ['max-gradient', 'mean-gradient', 'zero-gradient'])
         plt.savefig(os.path.join(output_dir, "gradient_flow.png"))
         plt.close()
 
